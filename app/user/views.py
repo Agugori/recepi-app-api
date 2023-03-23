def66d272a51afc0887818ea9f179f2d346d869f
u632913 <agori@teco.com.ar>
2023-03-23 17:44:18 -0300

    Test for users and serializer

---
+++ app/user/views.py
@@ -0,0 +1,12 @@
+"""
+View for the user API
+"""
+
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create new user in system"""
+    serializer_class = UserSerializer
